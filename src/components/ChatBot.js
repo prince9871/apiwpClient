37bad402126c93cbdeea12cc2ec5508f38b0b15c
Prince Soni <61613218+prince9871@users.noreply.github.com>
2023-11-06 19:31:46 +0530

    Add files via upload

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -28,56 +28,79 @@ const Chatbot = () => {
     setInputError(false);
   };
 
-  const sendMessage = async () => {
+  const sendMessage = () => {
     // Validate input
     if (input.trim() === '') {
       setInputError(true);
       return;
     }
-
-    setMessages([...messages, { text: input, user: true }]); // Display user's message immediately
-    setLoading(true); // Show loading animation while fetching data
-
-    setTimeout(async () => {
-      const response = await fetch('http://192.168.1.11:3001/api/messages', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message: input }),
-      });
-
-      const data = await response.json();
-      setMessages([...messages, { text: data.message, bot: true }]); // Display bot's response
-      setLoading(false); // Hide loading animation after receiving the response
+  
+    // Show loading animation
+    setLoading(true);
+  
+    // Process user input and provide appropriate suggestions
+    let botMessage = '';
+    if (input.toLowerCase() === 'hi') {
+      botMessage = 'Please type "a" or "b".';
+      setMessages([...messages, { text: input, user: true }, { text: botMessage, bot: true }]);
       setInput('');
-    }, 1000); // Delay the bot's response by 1 second
+      setLoading(false); // Hide loading animation after setting the suggestions
+      return;
+    }
+  
+    // For other inputs, make a request to the database and display the bot's response after 1 second
+    fetch('http://192.168.1.11:3001/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: input }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setTimeout(() => {
+          setMessages([...messages, { text: input, user: true }, { text: data.message, bot: true }]);
+          setLoading(false); // Hide loading animation after receiving the response
+          setInput('');
+        }, 1000); // Delay the bot's response by 1 second
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setLoading(false); // Hide loading animation in case of an error
+        setInput('');
+      });
   };
+  
+  
 
   return (
     <div className="chat-container">
-      
       <div className="messages">
         {messages.map((message, index) => (
-          <div key={index} className={message.bot ? 'message left-message' : 'message right-message'}>
+          <div
+            key={index}
+            className={
+              message.user ? 'message right-message' : message.bot ? 'message left-message' : ''
+            }
+          >
             {message.text}
           </div>
         ))}
         <div ref={messagesEndRef}></div>
       </div>
       {loading && <div className="loading">Loading...</div>}
+      {inputError && <div className="error-message">Please type a message.</div>}
       <div className="input-container">
-    <input
-      type="text"
-      value={input}
-      onChange={handleInputChange}
-      placeholder="Type your message..."
-      className={inputError ? 'input-error' : ''}
-    />
-    <button onClick={sendMessage}>Send</button>
-  </div>
-  {inputError && <div className="error-message">Please type a message.</div>}
-</div>
+        <input
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Type your message..."
+          className={inputError ? 'input-error' : ''}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
   );
 };
 
